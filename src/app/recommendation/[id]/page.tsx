c17e9fc7fc23dc75b07d2557442f2e454da2aeb9
haeunchoi-dev <haeunchoi.dev@gmail.com>
2023-06-25 01:19:00 +0900

    style: 공유 페이지 스타일링

--- src/app/recommendation/[id]/page.tsx
+++ src/app/recommendation/[id]/page.tsx
@@ -1,29 +1,42 @@
-export default function Page({ params }: { params: { id: string } }) {
+import styles from "./page.module.scss";
+
+interface PageProps {
+  params: { id: string };
+}
+
+export default function Page({ params }: PageProps) {
   return (
-    <main>
-      <div>
-        <header>
-          <h1>
-            유니크하면서 모던하고 대중적이지만 시크하고 유머러스한 닉네임 요청이
-            왔어요
+    <main className={styles.main}>
+      <div className={styles.pageLayout}>
+        <header className={styles.header}>
+          <h1 className={styles.title}>
+            직장에서 사용할 <br />
+            귀엽고 깜찍한데 만만치 않은 <br />
+            닉네임을 지어주세요.
           </h1>
+          <div className={styles.tags}>
+            <div className={styles.tag}>왕쎄보이게</div>
+            <div className={styles.tag}>그렇지만 귀엽게</div>
+            <div className={styles.tag}>초성이 들어가게</div>
+            <div className={styles.tag}>초성이 들어가게</div>
+            <div className={styles.tag}>초성이 들어가게</div>
+          </div>
         </header>
-        <section>
-          <div>
-            <h3>게임에서 사용할</h3>
-            <h2>귀엽고 깜찍하게 사랑스럽게</h2>
-            <div>
-              <div>짱쎄보이게</div> <div>하지만 귀여운</div>
-            </div>
+        <section className={styles.sendSection}>
+          <div className={styles.inputWrap}>
+            <input
+              type="text"
+              className={styles.input}
+              placeholder="닉네임을 지어주세요."
+            />
+            <button type="button" className={styles.inputClearBtn}>
+              x
+            </button>
           </div>
-        </section>
-        <section>
-          <div>
-            <input type="search" />
-          </div>
-          <div>
-            <button type="button">완료</button>
-          </div>
+
+          <button className={styles.sendButton} type="button">
+            전송
+          </button>
         </section>
       </div>
     </main>
