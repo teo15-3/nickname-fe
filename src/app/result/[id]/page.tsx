eb5f2914544f32c8a352c07f4ae9a468b867cdc4
MAGHC <windowsapple95@gmail.com>
2023-06-25 16:00:30 +0900

    feat : handleClipBoard 기능 생성 ,적용

--- src/app/result/[id]/page.tsx
+++ src/app/result/[id]/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import { useState } from "react";
+import { onKaKaoShare } from "../../../service/kakao";
+import { copy } from "../../../utils/clipboard";
+import Notification from "../../../components/result/Notification/Notification";
 
 type Props = {
   params: {
@@ -30,9 +33,31 @@ export default function Create({ params }: Props) {
     }, 2000);
   };
 
+  const handleClipBoardSelfBtn = () => {
+    const curPath = window.location.pathname;
+    const url = `${process.env.NEXT_PUBLIC_DOMAIN_URL}${curPath}`;
+    copy(url).then(() => showCopyResultUiHandle("self"));
+  };
+
+  const handleClipBoardShareDirectBtn = () => {
+    const recommendataionUrl = `${process.env.NEXT_PUBLIC_DOMAIN_URL}/recommendation/${params.id}`;
+    copy(recommendataionUrl).then(() => showCopyResultUiHandle("share"));
+  };
+
   return (
     <main>
       <p>결과 보는 페이지~</p>
+      <button type="button" onClick={() => onKaKaoShare(params.id)}>
+        공유 버튼
+      </button>
+      <button type="button" onClick={handleClipBoardSelfBtn}>
+        결과 페이지 저장하기
+      </button>
+
+      <button onClick={handleClipBoardShareDirectBtn} type="button">
+        공유 URL 복사하기
+      </button>
+      {show && <Notification text={copyMessage} />}
     </main>
   );
 }
