e258e8d66ad684e78bfdda31f75487614f59e5d5
MAGHC <windowsapple95@gmail.com>
2023-06-25 15:59:51 +0900

    feat: showCoptResultUiHandle 함수 생성

--- src/app/result/[id]/page.tsx
+++ src/app/result/[id]/page.tsx
@@ -1,4 +1,35 @@
-export default function Create() {
+"use client";
+
+import { useState } from "react";
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+type CopyStatus = "self" | "share";
+
+const DEFAULT_COPY_MESSAGE = "클립보드에 복사되었습니다";
+
+export default function Create({ params }: Props) {
+  const [show, setShow] = useState(false);
+  const [copyMessage, setCopyMessage] = useState(DEFAULT_COPY_MESSAGE);
+
+  const showCopyResultUiHandle = (coptStatus: CopyStatus) => {
+    setShow(true);
+
+    if (coptStatus === "self") {
+      setCopyMessage(`${"현재 페이지가"} ${DEFAULT_COPY_MESSAGE}`);
+    } else if (coptStatus === "share") {
+      setCopyMessage(`${"공유 페이지가"} ${DEFAULT_COPY_MESSAGE}`);
+    }
+
+    setTimeout(() => {
+      setShow(false);
+    }, 2000);
+  };
+
   return (
     <main>
       <p>결과 보는 페이지~</p>
