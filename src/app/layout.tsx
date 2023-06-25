6d3686e935ba98fe925808c25c5396444b8e0443
Mulgyeol <mulgyeoljung@gmail.com>
2023-06-26 00:15:13 +0900

    chore: env 설정, layout url 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,11 +6,11 @@ import "../styles/reset.scss";
 import Script from "next/script";
 import { kakaoInit } from "../service/kakao";
 
-export const metadata = {
-  title: "유니크하면서 모던하고 대중적이지만 시크하고 유러스한 닉네임 지어줘",
-  description:
-    "유니크하면서 모던하고 대중적이지만 시크하고 유러스한 닉네임 지어줘",
-};
+// export const metadata = {
+//   title: "유니크하면서 모던하고 대중적이지만 시크하고 유러스한 닉네임 지어줘",
+//   description:
+//     "유니크하면서 모던하고 대중적이지만 시크하고 유러스한 닉네임 지어줘",
+// };
 
 export default function RootLayout({
   children,
@@ -25,8 +25,17 @@ export default function RootLayout({
       />
       <head>
         <meta
+          property="og:title"
+          content="유니크하면서 모던하고 대중적이지만 시크하고 유러스한 닉네임 지어줘"
+        />
+        <meta property="og:type" content="website" />
+        <meta
           property="og:image"
           content="https://velog.velcdn.com/images/y00eunji/post/2a869576-b8f3-4471-b176-ca8364a5fb6a/image.png"
+        />
+        <meta
+          property="og:description"
+          content="유니크하면서 모던하고 대중적이지만 시크하고 유러스한 닉네임 지어줘"
         />
       </head>
       <body>{children}</body>
