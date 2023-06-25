75a20c08429e6c36c47e475542055624e27550dd
haeunchoi-dev <haeunchoi.dev@gmail.com>
2023-06-25 14:21:41 +0900

    feat: Data Fetching
    https://nextjs.org/docs/app/building-your-application/data-fetching/fetching
    
    Loading 및 Error Handling 추가
    https://nextjs.org/docs/app/building-your-application/routing/loading-ui-and-streaming
    https://nextjs.org/docs/app/building-your-application/routing/error-handling

--- src/app/recommendation/[id]/page.tsx
+++ src/app/recommendation/[id]/page.tsx
@@ -6,7 +6,21 @@ interface PageProps {
   params: { id: string };
 }
 
-export default function Page({ params }: PageProps) {
+async function getData() {
+  /* const res = await fetch("http://localhost:3000/mock/recommendation.json", {
+    cache: "force-cache",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  } */
+
+  return { title: "tt" };
+}
+
+export default async function Page({ params }: PageProps) {
+  const data = await getData();
+
   return (
     <main className={styles.main}>
       <div className={styles.pageLayout}>
