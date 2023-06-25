e246c8bcce1b33c85c2c9724e398b1cce89d070b
y00eunji <aine0227@naver.com>
2023-06-26 00:12:22 +0900

    chore: 오픈 그래프 이미지 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,10 @@ export default function RootLayout({
         onLoad={() => kakaoInit()}
       />
       <head>
-        <meta property="og:image" content="/assets/img/img_kakao_share.png" />
+        <meta
+          property="og:image"
+          content="https://velog.velcdn.com/images/y00eunji/post/2a869576-b8f3-4471-b176-ca8364a5fb6a/image.png"
+        />
       </head>
       <body>{children}</body>
     </html>
