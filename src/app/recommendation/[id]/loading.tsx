a1c8aaee4db78db7330fa49fe13d26161d44054f
haeunchoi-dev <haeunchoi.dev@gmail.com>
2023-06-25 23:27:42 +0900

    feat : 요청 loading 페이지

--- src/app/recommendation/[id]/loading.tsx
+++ src/app/recommendation/[id]/loading.tsx
@@ -1,3 +1,13 @@
+import styles from "./loading.module.scss";
+
 export default function Loading() {
-  return <div>Loading...</div>;
+  return (
+    <main>
+      <div className={styles.layout}>
+        <div className={styles.loading}>...</div>
+        <div className={styles.title}>로딩중</div>
+        <div className={styles.subTitle}>잠시만 기다려 주세요</div>
+      </div>
+    </main>
+  );
 }
