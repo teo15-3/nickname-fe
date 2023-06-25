9e245ab1c603d5cc97032524579f05b3806c8427
MAGHC <windowsapple95@gmail.com>
2023-06-25 22:12:36 +0900

    feat: reSult Page

--- src/app/result/[id]/page.tsx
+++ src/app/result/[id]/page.tsx
@@ -1,9 +1,15 @@
 "use client";
 
-import { useState } from "react";
+import Image from "next/image";
+import { useState, useEffect } from "react";
 import { onKaKaoShare } from "../../../service/kakao";
 import { copy } from "../../../utils/clipboard";
 import Notification from "../../../components/result/Notification/Notification";
+import style from "./page.module.scss";
+import Tags from "../../../components/result/Tags/Tags";
+import Lists from "../../../components/result/Lists/Lists";
+
+import { RESLUT_PAGE_CONSTANTS } from "../../../utils/resultPageConstant";
 
 type Props = {
   params: {
@@ -11,22 +17,65 @@ type Props = {
   };
 };
 
-type CopyStatus = "self" | "share";
+type NicknameResultT = {
+  category: string;
+  title: string;
+  nicknames: string[];
+  tags: string[];
+};
 
-const DEFAULT_COPY_MESSAGE = "클립보드에 복사되었습니다";
+const DEFAULT_DATA_RESULT_STATE: NicknameResultT = {
+  category: "",
+  title: "",
+  tags: [],
+  nicknames: [],
+};
+
+// 아더키를 useState에 넣을필요가..?근데 당장 생각안나서 일단진행
 
 export default function Create({ params }: Props) {
   const [show, setShow] = useState(false);
-  const [copyMessage, setCopyMessage] = useState(DEFAULT_COPY_MESSAGE);
 
-  const showCopyResultUiHandle = (coptStatus: CopyStatus) => {
+  const [data, setData] = useState(DEFAULT_DATA_RESULT_STATE);
+  const [otherKey, setOtherKey] = useState("");
+
+  const handleDeleteBtn = async (mykey: string, nickname: string) => {
+    const res = await fetch(
+      `https://api.fire-lighter.kr/nickname/${mykey}/nickname-received?nickname=${nickname}`,
+      {
+        method: "DELETE",
+      },
+    );
+    // 나머지 코드 작성
+  };
+
+  useEffect(() => {
+    const fetchResultData = async () => {
+      const res = await fetch(
+        `https://api.fire-lighter.kr/nickname/${params.id}/nickname-received`,
+      )
+        .then((res) => res.json())
+        .then(setData);
+    };
+
+    const fetchOtherKey = async () => {
+      const res = await fetch(
+        `https://api.fire-lighter.kr/key?mykey=${params.id}`,
+      )
+        .then((res) => res.json())
+        .then((res) => setOtherKey(res.otherkey));
+    };
+
+    fetchResultData();
+    fetchOtherKey();
+  }, []);
+
+  console.log(otherKey);
+
+  const { category, title, nicknames, tags } = data;
+
+  const showCopyResultUiHandle = () => {
     setShow(true);
-
-    if (coptStatus === "self") {
-      setCopyMessage(`${"현재 페이지가"} ${DEFAULT_COPY_MESSAGE}`);
-    } else if (coptStatus === "share") {
-      setCopyMessage(`${"공유 페이지가"} ${DEFAULT_COPY_MESSAGE}`);
-    }
 
     setTimeout(() => {
       setShow(false);
@@ -36,28 +85,81 @@ export default function Create({ params }: Props) {
   const handleClipBoardSelfBtn = () => {
     const curPath = window.location.pathname;
     const url = `${process.env.NEXT_PUBLIC_DOMAIN_URL}${curPath}`;
-    copy(url).then(() => showCopyResultUiHandle("self"));
+    copy(url).then(showCopyResultUiHandle);
   };
 
   const handleClipBoardShareDirectBtn = () => {
-    const recommendataionUrl = `${process.env.NEXT_PUBLIC_DOMAIN_URL}/recommendation/${params.id}`;
-    copy(recommendataionUrl).then(() => showCopyResultUiHandle("share"));
+    const recommendataionUrl = `${process.env.NEXT_PUBLIC_DOMAIN_URL}/recommendation/${otherKey}`;
+    copy(recommendataionUrl).then(showCopyResultUiHandle);
   };
 
   return (
-    <main>
-      <p>결과 보는 페이지~</p>
-      <button type="button" onClick={() => onKaKaoShare(params.id)}>
-        공유 버튼
-      </button>
-      <button type="button" onClick={handleClipBoardSelfBtn}>
-        결과 페이지 저장하기
+    <main className={style.wrapper}>
+      <section className={style.titles}>
+        <h3 className={style.title}>{`${category}`}</h3>
+        <h3 className={style.title}>{`${title}`}</h3>
+        <h3 className={style.title}>닉네임목록</h3>
+      </section>
+
+      <section className={style.tagSection}>
+        <Tags tags={tags} />
+      </section>
+
+      <section className={style.listUpContainer}>
+        {nicknames.length !== 0 ? (
+          <Lists
+            handleDelete={handleDeleteBtn}
+            mykey={params.id}
+            lists={nicknames}
+          />
+        ) : (
+          <p />
+        )}
+      </section>
+
+      <button
+        type="button"
+        onClick={handleClipBoardSelfBtn}
+        className={`${style.imgcontainer} ${style.shareBtn}`}
+      >
+        <Image
+          alt="shareResultSelf"
+          src={`${RESLUT_PAGE_CONSTANTS.DFAULT_IMG_PATH}bt_priamry_link copy.svg`}
+          width={224}
+          height={56}
+        />
       </button>
 
-      <button onClick={handleClipBoardShareDirectBtn} type="button">
-        공유 URL 복사하기
-      </button>
-      {show && <Notification text={copyMessage} />}
+      <section className={style.share}>
+        <h4 className={style.shareTitle}>닉네임 짓기 요청하기</h4>
+        <div className={style.buttons}>
+          <button
+            onClick={() => onKaKaoShare(otherKey)}
+            className={style.shareBtn}
+            type="button"
+          >
+            <Image
+              alt="kakaoShare"
+              src={`${RESLUT_PAGE_CONSTANTS.DFAULT_IMG_PATH}bt_kakao share.svg`}
+              width={166}
+              height={50}
+            />
+          </button>
+          <button
+            onClick={handleClipBoardShareDirectBtn}
+            className={style.shareBtn}
+            type="button"
+          >
+            <Image
+              alt="shareReulstLink"
+              src={`${RESLUT_PAGE_CONSTANTS.DFAULT_IMG_PATH}bt_link_share.svg`}
+              width={166}
+              height={50}
+            />
+          </button>
+        </div>
+      </section>
+      {show && <Notification />}
     </main>
   );
 }
