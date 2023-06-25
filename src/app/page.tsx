171d67ba39e2de75541855511a1cfdf7ea968847
y00eunji <aine0227@naver.com>
2023-06-25 19:48:42 +0900

    feat: 참여자 수 출력

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,18 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "./page.module.scss";
 
+import getIntroData from "../lib/api/intro/index";
+
 export default function Home() {
+  const getUserCount = async () => {
+    try {
+      const res = await getIntroData();
+      return res.userCount;
+    } catch (e) {
+      return 0;
+    }
+  };
+
   return (
     <main className={styles.main}>
       <div className={styles.introPageLayout}>
@@ -29,7 +40,8 @@ export default function Home() {
           </div>
           <div className={styles.footer}>
             <div className={styles.description}>
-              지금까지 <b className={styles.highlightText}>314</b>명이 <br />
+              지금까지 <b className={styles.highlightText}>{getUserCount()}</b>
+              명이 <br />
               유니크하고 모던하고 대중적이지만 시크하고 유머러스한 <br />{" "}
               닉네임을 얻어갔습니다.
             </div>
