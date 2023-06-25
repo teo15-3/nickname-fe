0d366eb4f687cbd6130314c10e53c2513d5cda75
MAGHC <windowsapple95@gmail.com>
2023-06-25 21:39:06 +0900

    fix: 모달 디자인 대로 수정

--- src/components/result/Notification/Notification.tsx
+++ src/components/result/Notification/Notification.tsx
@@ -1,13 +1,12 @@
 "use client";
 
+import Image from "next/image";
 import { useEffect, useState } from "react";
 import style from "./Notification.module.scss";
 
-type Props = {
-  text: string;
-};
+import { RESLUT_PAGE_CONSTANTS } from "../../../utils/resultPageConstant";
 
-const Notification = ({ text }: Props) => {
+const Notification = () => {
   const [isOpen, setisOpen] = useState(false);
 
   useEffect(() => {
@@ -19,7 +18,12 @@ const Notification = ({ text }: Props) => {
   return (
     <section className={style.notification}>
       <div className={`${style.textWrapper} ${isOpen ? style.show : ""} }`}>
-        {text}
+        <Image
+          width={338}
+          height={42}
+          alt="modal"
+          src={`${RESLUT_PAGE_CONSTANTS.DFAULT_IMG_PATH}toast_copy complete.svg`}
+        />
       </div>
     </section>
   );
