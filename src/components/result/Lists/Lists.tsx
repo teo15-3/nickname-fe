e348b166d63c8db84529a2b6478d8b673f3b539f
Mulgyeol <mulgyeoljung@gmail.com>
2023-06-27 00:44:45 +0900

    feat: 추천 받은 닉네임이 없으면 보여줄 문구 추가

--- src/components/result/Lists/Lists.tsx
+++ src/components/result/Lists/Lists.tsx
@@ -10,16 +10,26 @@ const Lists = ({
   lists: string[];
   mykey: string;
 }) => {
+  console.log(lists);
   return (
     <ul className={style.listUpContainer}>
-      {lists?.map((list, i) => (
-        <List
-          handleDelete={handleDelete}
-          mykey={mykey}
-          key={`${i.toString()}`}
-          list={list}
-        />
-      ))}
+      {lists.length > 0 ? (
+        lists.map((list, i) => (
+          <List
+            handleDelete={handleDelete}
+            mykey={mykey}
+            key={`${i.toString()}`}
+            list={list}
+          />
+        ))
+      ) : (
+        <div className={style.descContainer}>
+          <span className={style.desc}>
+            링크를 받은 사람이 추천을 하면
+            <br /> 목록이 보여요!
+          </span>
+        </div>
+      )}
     </ul>
   );
 };
